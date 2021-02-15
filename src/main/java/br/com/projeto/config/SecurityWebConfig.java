e4ad44b97b8ad61cb75a11931a3568dfb089735d
lucasrluz <lucasr.luzbr@gmail.com>
2021-02-15 16:20:52 -0300

    Configuração de permissões de acesso em diretórios

---
+++ src/main/java/br/com/projeto/config/SecurityWebConfig.java
@@ -0,0 +1,42 @@
+package br.com.projeto.config;
+
+import br.com.projeto.security.ProjetoDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityWebConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ProjetoDetailsService projetoDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers("/").hasRole("padrao")
+            .and()
+
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+            .and();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder
+                .userDetailsService(projetoDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    public static void main(String[]args){
+        System.out.println(new BCryptPasswordEncoder().encode("123"));
+    }
+
+}
